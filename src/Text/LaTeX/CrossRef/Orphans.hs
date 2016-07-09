b5e892214fba93644f790704d9fcec8c80add7a3
Hiromi ISHII <konn.jinro_at_gmail.com>
2016-07-10 00:32:16 +0900

    Removed generic-deriving package

--- src/Text/LaTeX/CrossRef/Orphans.hs
+++ src/Text/LaTeX/CrossRef/Orphans.hs
@@ -1,9 +1,7 @@
 {-# LANGUAGE DeriveAnyClass, DeriveGeneric, StandaloneDeriving #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module Text.LaTeX.CrossRef.Orphans () where
-import Generics.Deriving.Uniplate (Uniplate)
-import GHC.Generics               (Generic)
-import Text.LaTeX.Base.Syntax     (LaTeX (..))
+import GHC.Generics           (Generic)
+import Text.LaTeX.Base.Syntax (LaTeX (..))
 
 deriving instance Generic LaTeX
-deriving instance Uniplate LaTeX
